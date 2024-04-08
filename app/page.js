f4ba1810afe255fd26195e868d5b8f3b225b8f26
mariegrace <mariegracebmg@gmail.com>
2024-04-08 16:59:08 +0200

    Finish the project setup

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div></div>  
+    <div>
+      <Button>Suscribe</Button>
+    </div>  
   );
 }
