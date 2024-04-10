5fcd81c2529ffa1860d55e30cc171e699e9065dc
mariegrace <mariegracebmg@gmail.com>
2024-04-10 19:17:43 +0200

    Add the doctors image

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 
 function Hero() {
@@ -6,11 +7,14 @@ function Hero() {
   <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
       <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
-        <img
-          alt=""
-          src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+        <Image
+          alt="doctor"
+          width={800}
+          height={800}
+          src="/doctors.jpg"
           className="absolute inset-0 h-full w-full object-cover"
         />
+        
       </div>
 
       <div className="lg:py-24">
